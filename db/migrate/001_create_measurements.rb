3cbafa927ac85c9fbc8262ba3d5c2963e5243451
Nils Jonsson <nils@alumni.rice.edu>
2007-12-01 04:58:54 +0000

    Scaffold measurement resources

---
+++ db/migrate/001_create_measurements.rb
@@ -0,0 +1,15 @@
+class CreateMeasurements < ActiveRecord::Migration
+  def self.up
+    create_table :measurements do |t|
+      t.date    :accounting_date
+      t.string  :accounting_time_period, :limit => 1
+      t.integer :value
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :measurements
+  end
+end
---
+++ test/functional/measurements_controller_test.rb
@@ -0,0 +1,45 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class MeasurementsControllerTest < ActionController::TestCase
+  def test_should_get_index
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:measurements)
+  end
+
+  def test_should_get_new
+    get :new
+    assert_response :success
+  end
+
+  def test_should_create_measurement
+    assert_difference('Measurement.count') do
+      post :create, :measurement => { }
+    end
+
+    assert_redirected_to measurement_path(assigns(:measurement))
+  end
+
+  def test_should_show_measurement
+    get :show, :id => 1
+    assert_response :success
+  end
+
+  def test_should_get_edit
+    get :edit, :id => 1
+    assert_response :success
+  end
+
+  def test_should_update_measurement
+    put :update, :id => 1, :measurement => { }
+    assert_redirected_to measurement_path(assigns(:measurement))
+  end
+
+  def test_should_destroy_measurement
+    assert_difference('Measurement.count', -1) do
+      delete :destroy, :id => 1
+    end
+
+    assert_redirected_to measurements_path
+  end
+end
