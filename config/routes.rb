7cfb0313c3c28af1a88d3c785b7b3ed8d7cc017e
Nils Jonsson <nils@alumni.rice.edu>
2007-12-02 02:41:55 +0000

    Add spreadsheet mockup as app/views/measurements/spreadsheet.html.erb and map it to the root and as an overload of the measurements resource collection

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :measurements
-
+  map.resources :measurements, :collection => {:spreadsheet => :get}
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -27,7 +27,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => 'measurements', :action => 'spreadsheet'
 
   # See how all your routes lay out with "rake routes"
 
