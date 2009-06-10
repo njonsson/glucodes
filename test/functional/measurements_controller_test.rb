ee67cd16893d697644c479bf7f176674bdeab317
Nils Jonsson <nils@alumni.rice.edu>
2009-06-10 09:01:10 -0500

    Update scaffolding for Rails v2.3.2.1

--- test/functional/measurements_controller_test.rb
+++ test/functional/measurements_controller_test.rb
@@ -1,18 +1,18 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class MeasurementsControllerTest < ActionController::TestCase
-  def test_should_get_index
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:measurements)
   end
 
-  def test_should_get_new
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  def test_should_create_measurement
+  test 'should create measurement' do
     assert_difference('Measurement.count') do
       post :create, :measurement => {:at => '2007-11-30 22:53:30',
                                      :value => '100'}
@@ -21,22 +21,22 @@ class MeasurementsControllerTest < ActionController::TestCase
     assert_redirected_to measurement_path(assigns(:measurement))
   end
 
-  def test_should_show_measurement
+  test 'should show measurement' do
     get :show, :id => measurements(:one).id
     assert_response :success
   end
 
-  def test_should_get_edit
+  test 'should get edit' do
     get :edit, :id => measurements(:one).id
     assert_response :success
   end
 
-  def test_should_update_measurement
+  test 'should update measurement' do
     put :update, :id => measurements(:one).id, :measurement => { }
     assert_redirected_to measurement_path(assigns(:measurement))
   end
 
-  def test_should_destroy_measurement
+  test 'should destroy measurement' do
     assert_difference('Measurement.count', -1) do
       delete :destroy, :id => measurements(:one).id
     end
