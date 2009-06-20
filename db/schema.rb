6724e8692efad4523f07a4910f51a8f52501599b
Nils Jonsson <nils@alumni.rice.edu>
2009-06-19 21:26:54 -0500

    Rename migration according to newer time-based convention

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 1) do
+ActiveRecord::Schema.define(:version => 20071201045854) do
 
   create_table "measurements", :force => true do |t|
     t.datetime "at",                                                           :null => false
