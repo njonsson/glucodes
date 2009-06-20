6a2d5e686e3a07bc5c1a6472d0cb7c26dada7214
Nils Jonsson <nils@alumni.rice.edu>
2009-06-20 10:15:06 -0500

    Fix autofocus algorithm and enhance it to include links as a tertiary focus point

--- public/javascripts/autofocus.js
+++ public/javascripts/autofocus.js
@@ -1,6 +1,17 @@
-// Activates (focuses and selects) the appropriate control on a page, either a
-// control having the 'autofocus' CSS class or otherwise the first control
-// encountered.
+// Activates (focuses and selects) the appropriate focusable element on a page,
+// either an element having the 'autofocus' CSS class or the first control
+// encountered, or the first link encountered.
+function autofocus() {
+  var selectors = ['.autofocus', ':input[type!="hidden"]', 'a[href]'];
+  for (var s in selectors) {
+    var el = $(selectors[s]);
+    if (el.length > 0) {
+      $(el.get(0)).focus().select();
+      return;
+    }
+  };
+}
+
 $(document).ready(function() {
-  $($('.autofocus, :input[type!="hidden"]').get(0)).focus().select();
+  autofocus();
 });
