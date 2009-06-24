290f1496ee6989305b1b9c2fea79bfa7bb38347a
Nils Jonsson <nils@alumni.rice.edu>
2009-06-23 20:01:46 -0500

    Tune up date and time formats to be m/dd/yyyy instead of mm/dd/yyyy

--- config/initializers/formats.rb
+++ config/initializers/formats.rb
@@ -1,6 +1,14 @@
-Date::DATE_FORMATS[:html] = '<abbr title="%A">%a</abbr> %m/%d/%Y'
+DAY_OF_WEEK_FORMAT = '<abbr title="%A">%a</abbr>'
+
+def month_day_year_format(date_or_time)
+  "#{date_or_time.month}/%d/%Y"
+end
+
+Date::DATE_FORMATS[:html] = lambda { |date|
+  date.strftime "#{DAY_OF_WEEK_FORMAT} #{month_day_year_format date}"
+}
 Time::DATE_FORMATS[:html] = lambda { |time|
   ante_post = (time.hour < 12) ? 'ante' : 'post'
   am_pm = "<abbr title=\"#{ante_post} meridiem\">#{ante_post[0..0]}.m.</abbr>"
-  time.strftime "#{Date::DATE_FORMATS[:html]} %I:%M #{am_pm}"
+  time.strftime "#{DAY_OF_WEEK_FORMAT} #{month_day_year_format time} %I:%M #{am_pm}"
 }
