cf577f6c03ca49ed84b25515ee68b0aacc94681a
Nils Jonsson <nils@alumni.rice.edu>
2009-06-23 19:49:15 -0500

    Remove 'href' attributes of pagination anchors after giving them AJAX behavior

--- public/javascripts/measurements/index.js
+++ public/javascripts/measurements/index.js
@@ -1,14 +1,17 @@
 function wireUpAjaxPagination() {
-  $('.pagination a').click(function(eventObject) {
-    var href = $(eventObject.target).attr('href');
-    $('.content').load(href, '', function(responseText, textStatus, xmlHttpRequest) {
-      if (textStatus != 'success') return;
-      wireUpAjaxPagination();
-      var nextPage = $('a.next_page');
-      (nextPage.length > 0) ? nextPage.focus() : $('a.prev_page').focus();
+  $('.pagination a').each(function(i, el) {
+    el = $(el);
+    var href = el.attr('href');
+    el.click(function() {
+      $('.content').load(href, '', function(responseText, textStatus, xmlHttpRequest) {
+        if (textStatus != 'success') return;
+        wireUpAjaxPagination();
+        var nextPage = $('a.next_page');
+        (nextPage.length > 0) ? nextPage.focus() : $('a.prev_page').focus();
+      });
+      return false;
     });
-    return false;
-  });
+  }).attr('href', '#');
 }
 
 $(document).ready(function() {
