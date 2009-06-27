9571f1f511abffdf47f92e456f9ff153d0ced39c
Nils Jonsson <nils@alumni.rice.edu>
2009-06-27 12:31:27 -0500

    Include all models in record count assertions

--- test/functional/measurements_controller_test.rb
+++ test/functional/measurements_controller_test.rb
@@ -7,7 +7,7 @@ class MeasurementsControllerTest < ActionController::TestCase
   end
   
   test 'should handle GET index.html as expected' do
-    assert_no_difference 'Measurement.count' do
+    assert_no_difference %w(Measurement.count Aggregate.count) do
       get :index
       assert_template 'index.html.haml'
       assert_equal({}, flash)
@@ -17,7 +17,7 @@ class MeasurementsControllerTest < ActionController::TestCase
   end
   
   test "should handle GET index.html with 'page' parameter as expected" do
-    assert_no_difference 'Measurement.count' do
+    assert_no_difference %w(Measurement.count Aggregate.count) do
       get :index, 'page' => '2'
       assert_template 'index.html.haml'
       assert_equal({}, flash)
@@ -27,7 +27,7 @@ class MeasurementsControllerTest < ActionController::TestCase
   end
   
   test "should handle GET index.html with 'per_page' parameter as expected" do
-    assert_no_difference 'Measurement.count' do
+    assert_no_difference %w(Measurement.count Aggregate.count) do
       get :index, 'per_page' => '2'
       assert_template 'index.html.haml'
       assert_equal({}, flash)
@@ -37,7 +37,7 @@ class MeasurementsControllerTest < ActionController::TestCase
   end
   
   test 'should handle XHR GET index.html as expected' do
-    assert_no_difference 'Measurement.count' do
+    assert_no_difference %w(Measurement.count Aggregate.count) do
       xhr :get, :index
       assert_template '_measurements.html.haml'
       assert_equal({}, flash)
@@ -47,7 +47,7 @@ class MeasurementsControllerTest < ActionController::TestCase
   end
   
   test "should handle XHR GET index.html with 'page' parameter as expected" do
-    assert_no_difference 'Measurement.count' do
+    assert_no_difference %w(Measurement.count Aggregate.count) do
       xhr :get, :index, 'page' => '2'
       assert_template '_measurements.html.haml'
       assert_equal({}, flash)
@@ -57,7 +57,7 @@ class MeasurementsControllerTest < ActionController::TestCase
   end
   
   test "should handle XHR GET index.html with 'per_page' parameter as expected" do
-    assert_no_difference 'Measurement.count' do
+    assert_no_difference %w(Measurement.count Aggregate.count) do
       xhr :get, :index, 'per_page' => '2'
       assert_template '_measurements.html.haml'
       assert_equal({}, flash)
