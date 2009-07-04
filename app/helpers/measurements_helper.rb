4546e6e29fa605011b988b283bffeb00b8c6d0bf
Nils Jonsson <nils@alumni.rice.edu>
2009-07-04 11:22:46 -0500

    Extract view logic to a view helper

--- app/helpers/measurements_helper.rb
+++ app/helpers/measurements_helper.rb
@@ -2,4 +2,14 @@
 
 # Provides view helpers to MeasurementsController views.
 module MeasurementsHelper
+  
+  def date_time_column_header
+    header = ''
+    if @measurements.any? { |m| m.approximate_time? }
+      header << content_tag(:span, '(≈ Approximate) ', :class => 'approximate')
+    end
+    header << 'Date/Time'
+    header
+  end
+  
 end
--- test/unit/helpers/measurements_helper_test.rb
+++ test/unit/helpers/measurements_helper_test.rb
@@ -1,4 +1,28 @@
 require 'test_helper'
 
-class MeasurementsHelperTest < ActionView::TestCase
+module MeasurementsHelperTest
+  
+  class DateTimeColumnHeader < ActionView::TestCase
+    
+    include MeasurementsHelper
+    
+    test 'should return expected text when @measurements is empty' do
+      @measurements = []
+      assert_equal 'Date/Time', date_time_column_header
+    end
+    
+    test 'should return expected text when @measurements contains no approximate times' do
+      @measurements = [stub(:approximate_time? => false)]
+      assert_equal 'Date/Time', date_time_column_header
+    end
+    
+    test 'should return expected text when @measurements contains both an approximate time and an exact time' do
+      @measurements = [stub(:approximate_time? => true),
+                       stub(:approximate_time? => false)]
+      assert_equal '<span class="approximate">(≈ Approximate) </span>Date/Time',
+                   date_time_column_header
+    end
+    
+  end
+  
 end
